fe2f97165b16a929caac2b2f017d49ee3d52aed3
Wahyu Alif Aldika <aldika94wahyu@gmail.com>
2019-03-11 11:02:41 +0700

    setting.py update

--- gisportal_django/wsgi.py
+++ gisportal_django/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('C:/xampp/htdocs/gisportal_django')
+sys.path.append('C:/xampp/htdocs/gisportal_django/gisportal_django')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gisportal_django.settings.settings")
 
